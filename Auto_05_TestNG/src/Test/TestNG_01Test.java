98b96c7ef45bb2b15ba8346b44ff471d653400f8
Nga Pham <pham.nga.nhnd@gmail.com>
2022-05-22 11:35:35 +0700

    Comit code 05.22

---
+++ Auto_05_TestNG/src/Test/TestNG_01Test.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestNG_01Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
